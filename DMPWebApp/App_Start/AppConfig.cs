90a5791465e6bf454f8cc197cda5bbccf90e67a9
Đàm Quang Thịnh <thinhquangshin@gmail.com>
2021-09-09 09:04:04 +0700

    ok

--- DMPWebApp/App_Start/AppConfig.cs
+++ DMPWebApp/App_Start/AppConfig.cs
@@ -7,7 +7,7 @@ namespace DMPWebApp.App_Start
 {
     public class AppConfig
     {
-        public const string API_DOMAIN = "http://api.duocmyphamhaiduong.com";
+        public const string API_DOMAIN = "https://api.duocmyphamhaiduong.com";
         public const string MAIL_HOST = "thinhquangshin@gmail.com";
         public const string MAIL_PASS = "kamenrider";
         public const string MAIL_NAME = "Công ty TNHH Dược Mỹ Phẩm Hải Dương";
